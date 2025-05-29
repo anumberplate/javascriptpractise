110070f5b205efe8a91c566fb5f7aa1871841ab3
anumberplate <aluma.kyle@gmail.com>
2025-05-29 10:03:06 +0300

    While loops

--- control-flow.js
+++ control-flow.js
@@ -1,20 +1,20 @@
 let numbers = [1,2,3,4,5,6,7];
-
-let idx = 0;
-let lengthOfArray = numbers.length;
-
-//console.log(numbers[idx++]);
-//console.log(numbers[idx++]);
-//console.log(numbers[idx++]);
-//console.log(numbers[idx++]);
-//console.log(numbers[idx++]);
-//console.log(numbers[idx++]);
-//console.log(numbers[idx]);
-
 // for (let idx = 0; idx < numbers.length; idx++){
 //   console.log(numbers[idx]);
 // }
 
-for (let num = 0; num < 100; num++){
-  console.log(num);
+// let idx = 0;
+// while (idx < numbers.length){
+//   console.log(numbers[idx++]);
+// }
+
+let sum = 0;
+while (true){
+  console.log('loop');
+  sum ++;
+
+  if (sum == 10){
+    break;
+  }
+
 }
