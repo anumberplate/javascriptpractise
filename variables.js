e7ff20a1ad76c74c9273639241e1165bf9942a0b
anumberplate <aluma.kyle@gmail.com>
2025-05-29 06:33:46 +0300

    Primitive Types

--- variables.js
+++ variables.js
@@ -1,1 +1,22 @@
-const COLOR_GREEN  = 'green';
+let favouriteFruit =  'strawberries';
+let favouriteIceCream = "chocolate";
+let favouriteProgrammingLanguage = 'Javascript';
+
+let numberOfDonuts = 12;
+let pi = 3.14;
+
+let veryLargeNumber = 34343534535565363n;
+let lovesCoding = true;
+
+let favouriteColor;
+console.log(favouriteColor);
+
+favouriteFruit = null;
+
+const uniqueKey = Symbol();
+
+let course = {
+  name: 'JavaScript for beginners',
+  hours: 3
+};
+
