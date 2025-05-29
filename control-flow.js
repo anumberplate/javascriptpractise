1843908612697c0b6abe8e0e8c590f807f6c137b
anumberplate <aluma.kyle@gmail.com>
2025-05-29 10:44:19 +0300

    for of loops and for in loops

--- control-flow.js
+++ control-flow.js
@@ -1,5 +1,16 @@
-let i = 0;
-do {
-  console.log("Number");
-  i++;
-} while ( i < 10);
+const course = {
+  name: 'JavaScript for Beginners',
+  duration: 3,
+  sections:7
+};
+
+// console.log(course.name)
+
+for (const key in course){
+  console.log(course[key]);
+}
+
+let numbers = [0,1,2,3,4,5]
+for(let number of numbers){
+  console.log(number);
+}
