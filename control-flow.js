4258e6155a5f2fcc659e095b0a6d00ae60849d00
anumberplate <aluma.kyle@gmail.com>
2025-05-29 09:38:42 +0300

    Switch case

--- control-flow.js
+++ control-flow.js
@@ -1,20 +1,28 @@
-let priceOfChocolate = 1.99;
-let hasAmountInCash = 1.98;
+let job = 'Software Developer';
 
-const canBuyChocolate = hasAmountInCash >= priceOfChocolate;
+//if (job == 'Software Developer'){
+   // console.log('Writes code');
+//} else if (job == 'Designer'){
+//    console.log('Makes User interfaces');
+//} else if (job == 'Software Developer'){
+ //   console.log('Manages and deploys cloud resources');
+//} else{
+ //   console.log('Works directly with customers');
+//}
 
-if (canBuyChocolate){
-  console.log("Enjoy your purchase");
-} else{
-  console.log('Sorry you do not have enough');
-}
-
-let hour = 13;
-
-if ((hour>=6) && (hour<=12)) {
-  console.log("Serving breakfast");
-} else if(hour>12 && hour<=14){
-  console.log("Serving lunch");
-} else{
-  console.log("Serving dinner");
+switch(job){
+  case 'Software Developer':
+    console.log('Writes code');
+    break;
+  case 'Designer':
+    console.log('Makes User interfaces');
+    break;
+  case 'Cloud Engineer':
+    console.log('Writes code');
+    break;
+  case 'Software Developer':
+    console.log('Writes code');
+    break;
+  default:
+    console.log('Works directly with customers');
 }
